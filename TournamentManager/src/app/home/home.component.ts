4c8d22d021f8b1a07ff5d8334ddad4506404ead7
eliaspinna <eliaspinna99@gmail.com>
2024-06-26 16:25:30 +0200

    a

--- TournamentManager/src/app/home/home.component.ts
+++ TournamentManager/src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  tournaments: any[] = [];
+  filteredTournaments: any[] = [];
+  searchTerm: string = '';
 
+  constructor(private homeService: HomeService) {}
+
+  ngOnInit(): void {
+    this.getTournaments();
+  }
+
+  getTournaments(): void {
+    this.homeService.getAllTournaments().subscribe(response => {
+      this.tournaments = response.content;
+      this.filteredTournaments = this.tournaments;
+    });
+  }
+
+  onSearch(event: Event): void {
+    event.preventDefault();
+    this.filteredTournaments = this.searchTerm 
+      ? this.tournaments.filter(tournament => 
+          tournament.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+        )
+      : this.tournaments;
+  }
 }
